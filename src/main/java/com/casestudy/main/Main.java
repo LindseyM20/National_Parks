44dc412f322b3fc32a7de151b9978c3bbc867dfe
LindseyM20 <lindsey.bordner@gmail.com>
2021-03-27 15:16:56 -0500

    adds a UserService, refactors code to work with it

--- src/main/java/com/casestudy/main/Main.java
+++ src/main/java/com/casestudy/main/Main.java
@@ -1,17 +1,21 @@
 package com.casestudy.main;
 
-import com.casestudy.dao.UserDao;
 import com.casestudy.models.User;
+import com.casestudy.service.UserService;
 
 public class Main {
 
 	public static void main(String[] args) {
-		UserDao uDao = new UserDao();
+		UserService USer = new UserService();
 		
 		User u1 = new User("Lindsey", "hello@world.com", "asdf");
 		User u2 = new User("Dave", "funguy@fungi.com", "mush");
 		User u3 = new User("Jax", "jaxy@thedog.com", "treats");
-		uDao.addUser(u3);
+		User u4 = new User("Bonnie", "bon@thedog.com", "naps");
+		USer.addUserService(u1);
+		USer.addUserService(u2);
+		USer.addUserService(u3);
+		USer.addUserService(u4);
 
 	}
 }
